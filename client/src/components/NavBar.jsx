0b2c45b92e55cfd7836007e7f808cf20692fbb19
Nikhil <devsotnik@gmail.com>
2022-08-26 11:17:58 +0530

    test

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+
+function NavBar(){
+    return(
+        <>
+            <h1>Navbar s:</h1>
+        </>
+    )
+}
+
+export default NavBar;
--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "../node_modules/@fortawesome/fontawesome-free/css/all.css"
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
